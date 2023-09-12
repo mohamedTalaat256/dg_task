2b28d49994dd29e17a95463b1124520a6c680bb9
mohamedTalaat256 <76172138+mohamedTalaat256@users.noreply.github.com>
2023-09-12 09:58:10 +0300

    update dtos

--- src/main/java/com/example/dg_task/enumeration/ContactType.java
+++
@@ -1,5 +0,0 @@
-package com.example.dg_task.enumeration;
-
-public enum ContactType {
-    AID,B,P,UNKNOWN
-}
--- src/main/java/com/example/dg_task/enumeration/CountryCode.java
+++
@@ -1,9 +0,0 @@
-package com.example.dg_task.enumeration;
-
-public enum CountryCode {
-    AFN ,
-    DZD ,
-    AOA ,
-    EG,
-    TM
-}
