01c823776f4a478005d001aa0d7d896a2e4fce06
mohamedTalaat256 <76172138+mohamedTalaat256@users.noreply.github.com>
2023-09-13 12:21:32 +0300

    add logout

---
+++ frontend/src/app/model/user.model.ts
@@ -0,0 +1,11 @@
+export class User{
+    constructor(
+
+        public id: string,
+        public email: string,
+        public username: string,
+        public fullName: string,
+        public role: string[],
+        
+        ){}
+}
